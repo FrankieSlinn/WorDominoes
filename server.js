bd0199897a577b6cb313c260f045bf7ad641248e
Franziska Kissling <frankissling@yahoo.co.uk>
2023-08-15 21:24:09 +0100

    Deletes what more than hall of fame amount

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ const ejs = require('ejs');
 const path = require('path');
 const bodyParser= require('body-parser');
 const app = express();
+const cors = require('cors');
 const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index')
 const scoresRouter=require('./routes/scores')
 app.use(bodyParser.urlencoded({extended: true}))
+app.use(cors());
 app.use(express.json())
 app.use(express.static('./'));
 app.use(express.static(__dirname + '/public'));
+//prevent caching from server side in development. Can maybe remove in production. 
+app.set('view cache', false);
 const mdb = mongoose.connection;
 const MongoClient = require('mongodb').MongoClient;
 var qs = require('querystring');
@@ -49,17 +53,26 @@ app.set('views', __dirname + '/views');
     res.render('index.ejs');
   });
 
-  const Score = require('./models/score'); // Adjust the path to your score model file
+ const Score = require('./models/score'); 
   app.get('/scores', (req, res) => {
     Score.find().sort({ score: -1 })
       .then(results => {
-        res.render('scores.ejs', { scores: results });
+        res.render('scores.ejs', { scores: results }); // Render HTML for browser requests
       })
       .catch(err => {
         console.error('Error fetching scores from database:', err);
         res.status(500).send('Error fetching scores from database.');
       });
   });
+
+  
+  
+  
+  
+
+  
+  
+  
 app.use(indexRouter)
 app.use(scoresRouter)
 
